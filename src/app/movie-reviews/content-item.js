cbb02dd1822fe8b79720b34bb19e32870a23ae14
Nishant Aggarwal <naggarwal@wesleyan.edu>
2025-03-17 02:49:03 -0400

    movie-basics complete

---
+++ src/app/movie-reviews/content-item.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./movie-reviews.module.css"; // Import CSS module for styling
+
+const SingleEntry = ({ entry, onClick }) => {
+  return (
+    <div className={styles.entryContainer} onClick={onClick}>
+      <h2 className={styles.entryTitle}>{entry.title}</h2>
+      <h2 className={styles.entryTitle}>{entry.movie}</h2>
+      <h2 className={styles.entryTitle}>{entry.date}</h2>
+    </div>
+  );
+};
+
+export default SingleEntry;
--- src/app/movie-reviews/page.js
+++ src/app/movie-reviews/page.js
@@ -1,13 +1,58 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./movie-reviews.module.css";
+import { movieReviews } from "@/data/movie-reviews";
+import SingleEntry from "./content-item.js";
+import FullEntry from "./display-item";
 
-const About = () => {
+const Diary = () => {
+  const [selectedEntry, setSelectedEntry] = useState(null);
+
   return (
-    <div>
-      <h1>MOVIE REVIEWS</h1>
-      <p>This is the move-reviews page of our website.</p>
+    <div className={styles.diaryContainer}>
+      <div className={styles.contentsHeading}>MOVIE REVIEWS</div>
+
+      <div className={styles.diary}>
+        <div className={styles.textbox}>
+          {selectedEntry ? (
+            <>
+              {" "}
+              <FullEntry
+                entry={selectedEntry}
+                onBack={() => setSelectedEntry(null)}
+              />{" "}
+            </>
+          ) : (
+            <>
+              {movieReviews.map((entry, idx) => (
+                <SingleEntry
+                  key={idx}
+                  entry={entry}
+                  onClick={() => setSelectedEntry(entry)}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+
+      {selectedEntry && (
+        <div className={styles.pagination}>
+          {
+            <button
+              className={`${styles.pageButton} ${styles.backButton}`}
+              onClick={(e) => {
+                e.stopPropagation();
+                setSelectedEntry(null);
+              }}
+            >
+              &#x21A9;
+            </button>
+          }
+        </div>
+      )}
     </div>
   );
 };
 
-export default About;
+export default Diary;
