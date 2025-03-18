ad91092be68f7415a4f842273d7dd63379a6a423
Nishant Aggarwal <naggarwal@wesleyan.edu>
2025-03-18 03:02:05 -0400

    minor style fixes and images.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "wesleyanargus.com",
+        pathname: "/wp-content/uploads/**", // Allow images from this path
+      },
+      {
+        protocol: "https",
+        hostname: "wesleyanargus.com",
+        pathname: "/wp-content/uploads/**", // Ensure both HTTP & HTTPS are supported
+      },
+    ],
+  },
+};
 
 export default nextConfig;
